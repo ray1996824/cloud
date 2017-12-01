069c61a1f6826f3b4b39fdc80b43d47db307c2a0
tangbilly <tangbilly1129@gmail.com>
2017-12-01 17:36:46 +0800

    Update server.js

--- server.js
+++ server.js
@@ -41,6 +41,13 @@ app.get('/new',function(req,res){
   
 });
 
+/*
+Session
+*/
+app.use(session({
+    name: 'session',
+}));
+
 /** 
 * 
 *        Handle create restaurant
@@ -447,4 +454,68 @@ function insertRestaurantAPI(db,r,callback){
     callback(result,r._id);
   });
 }
+
+
+/*
+Rate
+*/
+app.post('/rate',function(req,res) {
+    var criteria = {
+        _id : ObjectId(req.rate._id)
+    };
+    console.log('Delete' + JSON.stringify(criteria));
+    MongoClient.connect(mongourl,function(err,db) {
+        assert.equal(err,null);
+        db.collection('restaurants').update(
+            { _id: ObjectId(req.rate._id) },
+            { $push:
+                { grades:
+                    {
+                        username:req.session.username,
+                        score:req.rate.score
+                    }
+                }
+            },function(err,result) {
+                assert.equal(err, null);
+                console.log("Rated");
+                res.redirect('/display?_id='+req.rate._id);
+            }
+
+        )
+    });
+});
+
+/*
+Register
+*/
+app.get('/register',function(req,res) {
+    res.sendFile(__dirname + '/public/register.html');
+});
+
+app.post('/register',function(req,res) {
+    MongoClient.connect(mongourl, function(err, db) {
+        assert.equal(err,null);
+        userRegister(db,req.body.username,req.body.password,function(result) {
+            db.close();
+            console.log('Disconnected MongoDB\n');
+            res.redirect('/');
+        });
+    });
+});
+
+/*
+Login
+*/
+app.get('/login',function(req,res) {
+    res.sendFile(__dirname + '/public/login.html');
+});
+
+/*
+Logout
+*/
+app.get('/logout',function(req,res) {
+    req.session = null;
+    res.redirect('/');
+});
+
 app.listen(app.listen(process.env.PORT || 8099 ));
